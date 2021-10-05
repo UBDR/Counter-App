075b2fa06690be424994aa55b10ea8b1964c0067
Ubaidur Rahman <syedanamrahman@gmail.com>
2021-10-05 14:35:22 +0530

    UI Updated

--- script.js
+++ script.js
@@ -6,22 +6,20 @@ display.innerHTML = 0;
 
 addButton.addEventListener('click', this.increaseDisplayData);
 subButton.addEventListener('click', this.decreaseDisplayData);
-let myWindow =  window.open("https://www.google.com","_blank");
+
 
 function increaseDisplayData() {
     display.innerHTML = Number(display.innerHTML) + 1;
-    myWindow.open();
+    
 
 
 }
 function decreaseDisplayData() {
     if(display.innerHTML>0)
     display.innerHTML = Number(display.innerHTML) - 1;
-    myWindow.close();
+  
 
 }
-// function openWindow(){
-//     window.open("www.google.com","_blank","width=200, height=100")
-// }
+
 
 
